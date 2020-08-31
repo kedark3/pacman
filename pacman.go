38490ece982f2a32a6d965fd9bcd6e0798bcc769
Kedar Vijay Kulkarni <kkulkarni@redhat.com>
2020-08-30 23:36:01 -0400

    Making port optional

--- pacman.go
+++ pacman.go
@@ -12,13 +12,17 @@ const (
 )
 
 func main() {
+	port := "80880"
+	if len(os.Args) != 0 {
+		port = string(os.Args[1])
+	}
 	fs := http.FileServer(http.Dir("static/"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/static/index.html", http.StatusSeeOther)
 	})
 
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	err := http.ListenAndServe(Host+":"+string(os.Args[1]), nil)
+	err := http.ListenAndServe(Host+":"+port, nil)
 	if err != nil {
 		log.Fatal("Error Starting the HTTP Server: ", err)
 		return
